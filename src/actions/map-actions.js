2a0f14f81d1e32fee5f89f0bb6be5ed8eb7ccee2
David Moodie <davidmoodie12@gmail.com>
2017-08-11 16:33:41 +0100

    Start dynamic ajax request for store info

--- src/actions/map-actions.js
+++ src/actions/map-actions.js
@@ -1,4 +1,5 @@
 import * as types from './action-types';
+// import rp from 'request-promise';
 
 export const setMapMarkers = (markers) => {
   return {
@@ -6,3 +7,26 @@ export const setMapMarkers = (markers) => {
     markers
   };
 }
+
+// export const setMapMarkers = (markers) => {
+//   const homepage = 'https://davidmoodie.com/react-leaflet-store-locator';
+//   const dataPath = 'data/stores-sky2.json';
+//   const options = {
+//     uri: `${homepage}/${dataPath}`,
+//     json: true // Automatically parses the JSON string in the response
+//   };
+//
+//   rp(options)
+//     .then(function (res) {
+//       console.log(res);
+//
+//       return {
+//         type: types.SET_MAP_MARKERS,
+//         markers
+//       };
+//     })
+//     .catch(function (err) {
+//       // API call failed...
+//       console.err("Error getting stores", err);
+//     });
+// }
